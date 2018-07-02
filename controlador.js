ce0e26bffae23dd77949dcd8ffc7cb6e0915cf0d
Lautaro-x <lautarohrodriguez@gmail.com>
2018-07-02 12:40:21 -0300

    111

---
+++ controlador.js
@@ -0,0 +1,27 @@
+
+window.onload = cargarselect();
+
+function cargarselect(){
+
+    ///Aca cargamos los elementos desde la base de datos...
+    var elementos = ["elemento 1", "elemento 2", "elemento 3", "elemento 4..."];
+
+    var select_elementos = document.getElementById("selector");
+    
+    elementos.forEach( function( nombre, indice )
+    {
+        opcion = document.createElement("option");
+        opcion.text = nombre;
+        select_elementos.add(opcion);
+    });
+}
+
+$(document).ready(function(){
+	$("#btn").click(
+        function(){
+            // Aca ponemos el numero que queremos.
+            var entero = parseInt(Math.random()*999999);
+            $("#texto").text("Su número es: " + entero);
+        }
+    );
+});
